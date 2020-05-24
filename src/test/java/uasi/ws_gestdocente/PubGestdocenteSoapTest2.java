fabf7c9fe6aa85f83df09a6689816ff668682dfb
Nick Carlos Mesias Rivera <nickmeza@upeu.edu.pe>
2020-05-24 13:12:04 -0500

    nick

--- src/test/java/uasi/ws_gestdocente/PubGestdocenteSoapTest2.java
+++ src/test/java/uasi/ws_gestdocente/PubGestdocenteSoapTest2.java
@@ -13,7 +13,6 @@ public class PubGestdocenteSoapTest2 {
 	public void testWsagrupaciones() {
 		gestdocent.wsagrupaciones("C", "2011-12", "B142", "G") ;
 	}
-
 	@Test
 	public void testWsfechaexamenesasi() {
 		gestdocent.wsfechaexamenesasi(plengua, pcodest, pcurso, pcodasi, pcodconvoc, porden) ;
@@ -22,17 +21,28 @@ public class PubGestdocenteSoapTest2 {
 	@Test
 	public void testWsfechaexamenesasitotal() {
 		gestdocent.wsfechaexamenesasitotal(plengua, pcodest, pcurso, pcodasi, pcodconvoc, porden) ;
-	}
-
-	@Test
-	public void testWsfechaexamenesasinue() {
-		gestdocent.wsfechaexamenesasinue(plengua, pcurso, pcodasi, pcodconv, porden);
-	}
-
-	@Test
-	public void testWsfechaexamenesplan() {
-		gestdocent.wsfechaexamenesplan(plengua, pcodest, pcurso, pcodconv, porden);
 	}*/
+
+	@Test
+	public void testWshorarios() {
+		System.out.println("\n\n\n\n\n");
+		ArrayOfClaseHorarios Horarios = gestdocent.wshorarios("C", "2010-11", "9244", "T");
+		List<ClaseHorarios> x = Horarios.getClaseHorarios();
+		for (ClaseHorarios claseHorarios : x) {
+			System.out.println(claseHorarios.getAula()+"\t"+claseHorarios.getAulasig()+"\t"+claseHorarios.getCapacidadgrupo()+"\t"+claseHorarios.getClaseActiv()+"\t"+claseHorarios.getCodgrp()+"\t"+claseHorarios.getDescactiv()+"\t"+claseHorarios.getDescaula()+"\t"+claseHorarios.getDescgrupo()+"\t"+claseHorarios.getDia()+"\t"+claseHorarios.getDiasemana()+"\t"+claseHorarios.getIdioma());
+		}
+		assertTrue(20==20);
+	}
+
+	@Test
+	public void testWscalificaciones() {
+		ArrayOfClaseCalificaciones Calificaciones = gestdocent.wscalificaciones("C", "9244");
+		List<ClaseCalificaciones> x = Calificaciones.getClaseCalificaciones();
+		for (ClaseCalificaciones claseCalificaciones : x) {
+			System.out.println(claseCalificaciones.getCod()+"\t"+claseCalificaciones.getDescripcion()+"\t"+claseCalificaciones.getRanmax()+"\t"+claseCalificaciones.getRanmin());
+		}
+		assertTrue(20==20);
+	}
 	@Test
 	public void testWsasidepto() {
 		ArrayOfClaseAsiDepto AsiDepto = gestdocent.wsasidepto("C", "2011-12", "B142", "");
