6af53178f99d36519d290bb8fc96e66c20b0408b
Nick Carlos Mesias Rivera <nickmeza@upeu.edu.pe>
2020-05-23 19:46:39 -0500

    nick

--- src/test/java/uasi/ws_gestdocente/PubGestdocenteSoapTest2.java
+++ src/test/java/uasi/ws_gestdocente/PubGestdocenteSoapTest2.java
@@ -3,6 +3,7 @@ package uasi.ws_gestdocente;
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -10,113 +11,35 @@ public class PubGestdocenteSoapTest2 {
 	PubGestdocenteSoap gestdocent = new PubGestdocente().getPubGestdocenteSoap();
 	/*@Test
 	public void testWsagrupaciones() {
-		gestdocent.wsagrupaciones("", "", "", "G") ;
+		gestdocent.wsagrupaciones("C", "2011-12", "B142", "G") ;
 	}
 
 	@Test
 	public void testWsfechaexamenesasi() {
-		fail("Not yet implemented");
+		gestdocent.wsfechaexamenesasi(plengua, pcodest, pcurso, pcodasi, pcodconvoc, porden) ;
 	}
 
 	@Test
 	public void testWsfechaexamenesasitotal() {
-		fail("Not yet implemented");
+		gestdocent.wsfechaexamenesasitotal(plengua, pcodest, pcurso, pcodasi, pcodconvoc, porden) ;
 	}
 
 	@Test
 	public void testWsfechaexamenesasinue() {
-		fail("Not yet implemented");
+		gestdocent.wsfechaexamenesasinue(plengua, pcurso, pcodasi, pcodconv, porden);
 	}
 
 	@Test
 	public void testWsfechaexamenesplan() {
-		fail("Not yet implemented");
-	}
-
+		gestdocent.wsfechaexamenesplan(plengua, pcodest, pcurso, pcodconv, porden);
+	}*/
 	@Test
 	public void testWsasidepto() {
-		fail("Not yet implemented");
+		ArrayOfClaseAsiDepto AsiDepto = gestdocent.wsasidepto("C", "2011-12", "B142", "");
+		List<ClaseAsiDepto> x = AsiDepto.getClaseAsiDepto();
+		for (ClaseAsiDepto claseAsiDepto : x) {
+			System.out.println(claseAsiDepto.getCodasi()+"\t"+claseAsiDepto.getCodest()+"\t"+claseAsiDepto.getNomasi()+"\t\t"+claseAsiDepto.getNomest());
+		}
+		assertTrue(20==20);
 	}
-
-	@Test
-	public void testWsdatosasig() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testWshorarioagrp() {
-		fail("Not yet implemented");
-	}*/
-
-	@Test
-	public void testWshorarios() {
-		gestdocent.wshorarios("C", "2012-11", "2", "T");
-		
-	}
-
-	/*@Test
-	public void testWstitulosuni() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testWsdatosplan() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testWsplanesasig() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testWsplanesdep() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testWsdptoasig() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testWsareasdpto() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testWsdptocen() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testWsasiplan() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testWscalificaciones() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testWsconvocatorias() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testWsgruposasi() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testWsconvemovilidad() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testWstitulospropiosuni() {
-		fail("Not yet implemented");
-	}*/
-
 }
